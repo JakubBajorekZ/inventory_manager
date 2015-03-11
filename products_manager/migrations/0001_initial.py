4f218d4aabf15585f72f36476aac12cc9727daf5
JakubBajorekZ <bajorek.jakub@gmail.com>
2015-03-11 17:33:10 +0100

    Add products manager models & migrations

---
+++ products_manager/migrations/0001_initial.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('description', models.TextField()),
+                ('quantity', models.IntegerField()),
+                ('price', models.DecimalField(max_digits=18, decimal_places=2)),
+                ('value', models.DecimalField(max_digits=18, decimal_places=2)),
+                ('pub_date', models.DateTimeField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ProductGroup',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='product',
+            name='product_group',
+            field=models.ForeignKey(to='products_manager.ProductGroup'),
+            preserve_default=True,
+        ),
+    ]
--- products_manager/models.py
+++ products_manager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class ProductGroup(models.Model):
+    name = models.CharField(max_length=200)
+
+class Product(models.Model):
+    product_group = models.ForeignKey(ProductGroup)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=18, decimal_places=2)
+    value = models.DecimalField(max_digits=18, decimal_places=2)
+    pub_date = models.DateTimeField()
+
+
+

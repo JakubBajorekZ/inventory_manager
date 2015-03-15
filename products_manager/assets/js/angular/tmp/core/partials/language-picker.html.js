5167a3485c114af04f7b4112606a01e16a58ad04
JakubBajorekZ <bajorek.jakub@gmail.com>
2015-03-15 04:12:58 +0100

    Remove default Angular components

--- products_manager/assets/js/angular/tmp/core/partials/language-picker.html.js
+++
@@ -1,4 +0,0 @@
-angular.module('mainModule').run(['$templateCache', function($templateCache) {
-    $templateCache.put('core/partials/language-picker.html',
-        "<div class=\"text-center\">\n    <h2>{{ 'label_which_language_do_you_prefer' | translate }}</h2>\n    <button class=\"btn btn-lg btn-default\" ui-sref-active=\"btn-success\" ui-sref=\"index.english\" translate=\"button_text_en\"></button>\n    <button class=\"btn btn-lg btn-default\" ui-sref-active=\"btn-success\" ui-sref=\"index.swedish\" translate=\"button_text_sv\"></button>\n</div>");
-}]);

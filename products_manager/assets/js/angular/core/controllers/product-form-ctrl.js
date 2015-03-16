29b290efebc549613a353af99d14a3bd2dedca59
JakubBajorekZ <bajorek.jakub@gmail.com>
2015-03-16 17:58:36 +0100

    Add user info about errors&watchers to compute total value in product form controller

--- products_manager/assets/js/angular/core/controllers/product-form-ctrl.js
+++ products_manager/assets/js/angular/core/controllers/product-form-ctrl.js
@@ -5,12 +5,29 @@ angular.module('mainModule')
       '$stateParams',
       'ProductFactory',
       function ($scope, $state, $stateParams, ProductFactory) {
+        
+      product_template = {
+        name: '---',
+        description: '---',
+        quantity: 0,
+        price: 0
+      };
       
-      $scope.new_product = {};
+      $scope.new_product = product_template;
+                
+      $scope.$watch('new_product', function(new_value, old_value){
+        compute_total_value($scope.new_product);
+      }, true);
+      
+      $scope.$watch('selected_product', function(new_value, old_value){
+        compute_total_value($scope.selected_product);
+      }, true);
       
       if($state.current.name == 'index.products.product_form'){
         ProductFactory.getProduct($stateParams.product_id).then(function (selected_product) {
           $scope.selected_product = selected_product;
+        }, function(cause){
+          alert('Nie udało się pobrać danych o produktach');
         });
       }
       
@@ -28,6 +45,8 @@ angular.module('mainModule')
           $scope.selected_product = updated_product;
           $scope.$parent.refresh();
           alert('Zaktualizowano dane produktu');
+        }, function(cause){
+          alert('Nie udało się zaktualizować danych produktu');
         });
       };
       
@@ -36,6 +55,8 @@ angular.module('mainModule')
           $state.go('index.products', { product_group_id: $stateParams.product_group_id });
           $scope.$parent.refresh();
           alert('Usunięto produkt');
+        }, function(cause){
+          alert('Nie udało się usunąć produktu z bazy');
         });
       };
       
